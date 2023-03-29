f0b7b0c762c05da76cb69bf687564e876437b348
Joseph Abdelmalak <42245728+jabdelmalak@users.noreply.github.com>
2023-03-29 13:27:57 -0400

    Update setup.sh

--- PFC/setup.sh
+++ PFC/setup.sh
@@ -15,7 +15,7 @@ tar -xvf $(pwd)/noderedvolume.tar  -C /
 
 echo "running node-red container"
 docker run --restart unless-stopped -d -p 1880:1880 --name node-red \
---network=host --security-opt seccomp:unconfined \
+--security-opt seccomp:unconfined \
 -v node_red_user_data:/data nodered/node-red:2.2.2
 
 chmod 777 /home/docker/volumes/node_red_user_data/_data
